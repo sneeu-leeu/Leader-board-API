fdd5f4367e59a16e4f923ba8ca17e8f57c08f35e
sneeu-leeu <sa070197@gmail.com>
2021-08-04 12:21:32 +0200

    move li generate into api with await & create refresh function

--- src/api.js
+++ src/api.js
@@ -1,10 +1,33 @@
 const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 const addGame = async () => {
-
   if (localStorage.getItem('gameId'));
 
-  const game = {
+  const newGame = {
     name: 'Footie with the boys',
   };
+
+  const response = await fetch(`${apiUrl}games/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newGame),
+  });
+  const data = await response.json();
+  const gameId = data.result.split(' ')[3];
+  localStorage.setItem('gameId', gameId);
+};
+
+const refresh = async () => {
+  const gameId = localStorage.getItem('gameId');
+  const list = document.getElementById('JS-score-log');
+  list.innerHTML = '';
+  const response = await fetch(`${apiUrl}games/${gameId}/scores`);
+  const scores = await response.json();
+  scores.result.forEach((score) => {
+    const listItem = document.createElement('li');
+    listItem.innerText = `${score.user}: ${score.score}`;
+    listItem.append(list);
+  });
 };
--- src/index.js
+++ src/index.js
@@ -10,15 +10,6 @@ const render = () => {
   leaderBoard.innerHTML = '';
   const sortedTask = logBoard.sort((a, b) => a.index - b.index);
 
-  sortedTask.forEach((score) => {
-    const listItem = document.createElement('li');
-    listItem.id = leaderBoard.length + 1;
-    listItem.classList.add('flex', 'aic-start');
-    const textElem = document.createElement('p');
-    textElem.classList.add('flex', 'aic-start');
-    textElem.append(score.player, score.hScore);
-    listItem.append(textElem);
-  });
 };
 
 document.addEventListener('DOMContentLoaded', render);
