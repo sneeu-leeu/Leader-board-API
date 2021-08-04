0824110e942d95b93efa736ca38c1e1eadbd1ee9
sneeu-leeu <sa070197@gmail.com>
2021-08-04 11:55:05 +0200

    Add and set up API url

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const addGame = async () => {
+
+  if (localStorage.getItem('gameId'));
+
+  const game = {
+    name: 'Footie with the boys',
+  };
+};
